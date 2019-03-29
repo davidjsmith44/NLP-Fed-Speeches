f4c7d63d52f27e5f472df14330b18f201c1f908a
Dave Smith <davidjsmith44@yahoo.com>
2019-03-29 14:59:53 -0700

    time_series_anal

--- src/first_model_pipeline.py
+++ src/first_model_pipeline.py
@@ -4,11 +4,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import quandl
+from sklearn.decomposition import PCA
 
 
 
-
-def plot_3_pcs(comps, shocks):
+def plot_3_pcs(comps, shocks, filename=None):
     ''' TO DO ON THIS PLOT
         1. make the X axis for charts on right the date
         2. optional title with start and end dates
@@ -45,6 +45,11 @@ def plot_3_pcs(comps, shocks):
     fig.suptitle("Principal Components of the US Treasury Yield Curve", fontsize = 30, color = 'k')
     fig.show()
 
+    if filename:
+        plt.savefig(filename)
+
+
+
 def sample_from_data(X, start_date, end_date):
     start_date = '2010-01-01'
     end_date ='2016-01-01'
@@ -67,9 +72,9 @@ def run_model_on_sample(X_samp, n_comp, random_state=44):
 
 
     # recovering the components (vectors)
-    component_vectors = test.components_
-    pct_var_explained = test.explained_variance_ratio_
-    shocks = test.fit_transform(X)
+    component_vectors = pca.components_
+    pct_var_explained = pca.explained_variance_ratio_
+    shocks            = pca.fit_transform(X)
 
     return_dict = {'component_vectors': component_vectors,
                     'pct_var_explained': pct_var_explained,
@@ -107,5 +112,30 @@ n_comp = 5
 
 
 pca_dict = run_model_on_sample(X_samp, 5)
+f_name= '../principal_components.png'
+#plot_3_pcs(pca_dict['component_vectors'], pca_dict['shocks'])
+plot_3_pcs(pca_dict['component_vectors'], pca_dict['shocks'], f_name)
 
-plot_3_pcs(pca_dict[component_vectors], pca_dict[shocks])
+# NOW LETS DO AUTOCORRELATION STUFF ON THE HISTORICAL SHOCKS
+# start looking at the shocks
+import statsmodels as sm
+# test = sm.tsa.stattools.adfuller(series)
+
+def series_and_lagged(series, lag=1):
+    truncated = np.copy(series)[lag:]
+    lagged = np.copy(series)[:(len(truncated))]
+    return truncated, lagged
+
+def compute_autocorrelation(series, lag=1):
+    series, lagged = series_and_lagged(series, lag=lag)
+    return np.corrcoef(series, lagged)[0, 1]
+
+def plot_series_and_lagged(axs, series, title, lag=1):
+    series.iloc[:-lag].plot(ax=axs[0], marker='.')
+    series.iloc[lag:].plot(ax=axs[1], marker='.')
+    axs[0].set_title(title)
+    axs[1].set_title(title + ' lagged by {}'.format(lag))
+
+fig, axs = plt.subplots(2, figsize=(14, 4))
+plot_series_and_difference(axs, google_trends['baseball'], 'baseball')
+fig.tight_layout()
